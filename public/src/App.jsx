d8ae600d0f5113d890fad3fd553f7ebb198e8a30
jimmy <wangzhiming@didichuxing.com>
2017-06-14 22:47:39 +0800

    init structure

---
+++ public/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import route from './Config/Route'; //路由配置
+import store from './Config/Store';
+
+
+import 'normalize.css'; //重置浏览器默认样式
+import 'flex.css'; //flex布局
+import './Style/style.less'; //加载公共样式
+import './Iconfont/iconfont.css'; //字体图标
+import 'github-markdown-css'; //markdown css
+
+if ( __DEV__ ){
+  console.log("现在是开发环境")
+}
+
+if (__PROD__) {
+  console.log("现在是生产环境")
+}
+
+
+render(
+    <Provider store={store}>
+        {route}
+    </Provider>,
+    document.body.appendChild(document.createElement('div'))
+);
